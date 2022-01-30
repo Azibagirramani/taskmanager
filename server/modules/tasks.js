85c3b20f150f89f5c8d1c8c5c91563aae2a3b922
Azibagirramani <kelvinmansi2@gmail.com>
2022-01-30 16:51:22 +0100

    tasks: error handling

--- server/modules/tasks.js
+++ server/modules/tasks.js
@@ -2,41 +2,38 @@ const Task = require("../modals/task");
 const Validator = require("../utilities/validations/task");
 
 exports.all = async function (req, res) {
-  const data = await Task.find();
-  res.json(data);
-};
-
-exports.add = async function (req, res) {
-  const { errors } = Validator(req.body);
-
-  // validates request body
-  if (!errors) return res.status(400).json(errors);
-
   try {
-    const response = await Task.create(req.body);
-
-    console.log(req.body);
-    res.json(response);
-    return;
+    const data = await Task.find();
+    res.json(data);
   } catch (error) {
-    res.status(500).json("unable to process request");
+    res.status(500).json({msg: "unable to process request"});
   }
 };
 
+exports.add = async function (req, res, next) {
+  const { errors } = Validator(req.body);
 
-exports.patch = function(req, res){
+  // validates request body
+  if (!errors.length == 0) return res.status(400).json(errors);
 
-   
+  try {
+    const response = await Task.create(req.body);
+    res.json(response);
+    return;
+  } catch (error) {
+    res.status(500).json({msg: "unable to process request"});
+  }
+};
 
-}
+exports.patch = function (req, res) {};
 
-exports.delete = async function(req, res){
-   const { id } = req.params
+exports.delete = async function (req, res) {
+  const { id } = req.params;
 
-   try {
-      const remove = await Task.deleteOne({_id: id })
-      res.json({ count: remove.deletedCount })      
-   } catch (error) {
-      res.status(500).json("unable to process request");
-   }
-}
+  try {
+    const remove = await Task.deleteOne({ _id: id });
+    res.json({ count: remove.deletedCount });
+  } catch (error) {
+    res.status(500).json({msg: "unable to process request"});
+  }
+};
